54d14e46dca91efd2bca59b90cad455e76cf94d5
Alan Schmollgruber <alanschmollgruber@gmail.com>
2020-06-12 09:02:22 +0200

    First control flow of entities for AddressBook.

---
+++ src/com/jemmic/interview/round2/AddressBookManagement.java
@@ -0,0 +1,180 @@
+package com.jemmic.interview.round2;
+
+import java.io.StringReader;
+import java.util.Scanner;
+
+public class AddressBookManagement {
+
+    // 0. Define basic fields and methods
+    public boolean processFinalized = false;
+    public Scanner scanner = new Scanner(System.in);
+
+    // 1. Define the address book
+    AddressBook addressBook = new AddressBook();
+
+    // 2. Define the methods
+    public void runApp() {
+        while(!processFinalized){
+            String generalMessage = "Please select one of the following options by typing in the respective number:\n" +
+                    "0. cancel\n" +
+                    "1. Add new contact\n" +
+                    "2. Display all contacts\n" +
+                    "3. Remove one specific contact\n" +
+                    "4. Edit one specific contact\n";
+
+
+            int optionSelected = scanIntegerMethod(generalMessage);
+
+            if(optionSelected != -1) {
+                System.out.println("You selected option: " + optionSelected + ".");
+
+                switch (optionSelected) {
+                    case 0:
+                        System.out.println("The process was canceled.");
+                        break;
+                    case 1:
+                        this.addContact();
+                        break;
+                    case 2:
+                        System.out.println();
+                        break;
+                    case 3:
+                        System.out.println();
+                        break;
+                    case 4:
+                        System.out.println();
+                        break;
+                }
+
+            } else {
+                System.out.println("The input is not correct.");
+            }
+        }
+    }
+
+    // function for scanning numbers
+    public int scanIntegerMethod(String message) {
+        String input;
+        int inputNumber = 0;
+        boolean scanCorrect = false;
+
+        while(!scanCorrect){
+            try {
+                System.out.println(message);
+                input = scanner.nextLine();
+                inputNumber = Integer.parseInt(input);
+                scanCorrect = true;
+
+                if(inputNumber<0){
+                    inputNumber = -1;
+                }
+            } catch (NumberFormatException exception){
+                System.err.println("The entered number is not an integer.");
+                scanCorrect = false;
+            }
+        }
+
+        return inputNumber;
+    }
+
+    // function for scanning numbers
+    public String scanStringsMethod(String instructionMessage, String outputMessage) {
+        String inputString = "";
+        boolean scanCorrect = false;
+
+        while(!scanCorrect){
+            System.out.println(instructionMessage);
+            inputString = scanner.nextLine();
+
+            if(! inputString.equals("")){
+                scanCorrect = true;
+            }
+        }
+
+        System.out.println(outputMessage + inputString +".");
+        return inputString;
+    }
+
+    // function for scanning numbers
+    public boolean scanAcceptDeclineMethod(String instructionMessage) {
+        String inputString = "";
+        boolean scanCorrect = false;
+        boolean acceptOrDecline = false;
+
+        while(!scanCorrect){
+            System.out.println(instructionMessage);
+            inputString = scanner.nextLine();
+
+            if(( inputString.equals("y") || inputString.equals("n"))){
+
+                if(inputString.equals("y")){
+                    acceptOrDecline = true;
+                }
+                scanCorrect = true;
+            }
+        }
+
+        return acceptOrDecline;
+    }
+
+    public void addContact(){
+        System.out.println( "You selected option 1: Add contact to address-book\n" +
+                            "Please type in the following mandatory input fields:");
+
+        // mandatory fields
+        String name = scanStringsMethod("Please type in the name:", "The typed in name is: ");
+        String surname = scanStringsMethod("Please type in the surname:", "The typed in surname is: ");
+        String telephoneNumber = scanStringsMethod("Please type in the telephone number:", "The typed in telephone number is: ");
+        String mail = scanStringsMethod("Please type in the email-address:", "The typed in email-address is: ");
+
+        // optional fields
+        boolean typeInAge = scanAcceptDeclineMethod("Would you like to save the age?\n Type in 'y' for yes or 'n' for no");
+        int age = -1;
+        if(typeInAge){
+            age = scanIntegerMethod("Please type in the age:");
+        }
+
+        boolean typeInHair = scanAcceptDeclineMethod("Would you like to save the hair-type?\n Type in 'y' for yes or 'n' for no");
+        String hair = "";
+        if(typeInHair){
+            hair = scanStringsMethod("Please type in the hair-type:", "The hair-type is: ");
+        }
+
+        // select contact-type
+        String contactTypeMessage =
+                "The contact belongs to one of the following categories:\n" +
+                "(select one of the options by typing in the respective number):\n" +
+                "1. acquaintance\n" +
+                "2. family\n" +
+                "3. friends\n";
+
+        int category = scanIntegerMethod(contactTypeMessage);
+
+        if(category != -1) {
+            System.out.println("You selected option: " + category + ".");
+
+            switch (category) {
+                case 1:
+                    System.out.println("acquaintance");
+                    this.addressBook.addEntryAcquaintance(name, surname, telephoneNumber, mail, ag);
+                    break;
+                case 2:
+                    System.out.println("family");
+                    break;
+                case 3:
+                    System.out.println("friend");
+
+
+
+
+                    break;
+            }
+        } else {
+            System.out.println("The input is not correct.");
+        }
+
+
+    }
+
+
+}
--- src/com/jemmic/interview/round2/Main.java
+++ src/com/jemmic/interview/round2/Main.java
@@ -3,7 +3,9 @@ package com.jemmic.interview.round2;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Address-Book-App description: ");
+        AddressBookManagement app = new AddressBookManagement();
+        app.runApp();
 
     }
 }
